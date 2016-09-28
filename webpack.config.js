55fdc8c3f5da4aeacae2424614a82750f4857b05
Ethan Gregory Clark <ethangclark@gmail.com>
2016-09-27 19:55:28 -0600

    Re-add webpack.config.js (was lost somehow)

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  entry: {
+    js: './src/index.js'
+  },
+  output: {
+    path: './lib',
+    filename: 'index.js',
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+      loaders: [
+          {
+              test: /\.js$/,
+              exclude: /node_modules/,
+              loader: 'babel',
+              query: {
+                  presets: ['es2015'],
+              }
+          }
+      ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  externals: {
+    react: true,
+    'object-assign': true
+  }
+}
