5524c5842f0717529ea358c5c58af3cbd381ab87
Ethan Gregory Clark <ethangclark@gmail.com>
2016-09-27 23:35:32 -0600

    Update Grid example

--- test/Grid/index.jsx
+++ test/Grid/index.jsx
@@ -1,37 +1,27 @@
 import { Provider, logAction} from '../../src'
 
+const isMinSize = ({rows, columns}) => rows.length === 1 && columns.length === 1
+
 const stateDef = {
 
   //values
   rows: [null, null, null],
   columns: [null, null, null],
-  gridClearIntervalID: null,
 
   //synchronous methods
   addRow: (_, state) => ({ rows: [].concat(state.rows, [null]) }),
   addColumn: (_, state) => ({ columns:[].concat(state.columns, [null]) }),
   removeRow: (_, state) => ({ rows: state.rows.slice(0, -1) }),
   removeColumn: (_, state) => ({ columns: state.columns.slice(0, -1) }),
-  setGridClearIntervalID: (intervalID, state) => ({ intervalID: intervalID }),
-
-  clearActiveGridClearInterval: (_, state) => { clearInterval(state.intervalID) },
 
   //asyncronous methods
-  clearActiveGridClearIntervalIfEmpty: (_, state) => {
-    state.rows.length || state.columns.length || state.clearActiveGridClearInterval()
+  _gridClearTick: (_, state) => {
+    state.rows.length > 1 && state.removeRow()
+    state.columns.length > 1 && state.removeColumn()
   },
-  gridClearTick: (_, state) => {
-    state.removeRow()
-    state.removeColumn()
-  },
-  startGridClear: (_, state) => {
-    state.clearActiveGridClearInterval()
-
-    const intervalID = setInterval(() => {
-      state.gridClearTick()
-      state.clearActiveGridClearIntervalIfEmpty()
-    }, 100)
-    state.setGridClearIntervalID(intervalID)
+  gridClear: (_, state) => {
+    state._gridClearTick()
+    if ( !isMinSize(state) ) setTimeout(() => state.gridClear(), 100)
   }
 }
 
@@ -40,15 +30,15 @@ const Grid = (props) => {
     <div className="grid">
       <div onClick={props.addRow}>Add row</div>
       <div onClick={props.addColumn}>Add column</div>
-      <div onClick={props.startGridClear}>Clear grid</div>
+      <div onClick={props.gridClear}>Clear grid</div>
       {
         props.rows.map((_, rowIndex) => (
           <div key={rowIndex}>
-          {
-            props.columns.map((_, columnIndex) => (
-              '(' + rowIndex + ':' + columnIndex + ')'
-            )).join(' ')
-          }
+            {
+              props.columns.map((_, columnIndex) => (
+                '(' + rowIndex + ':' + columnIndex + ')'
+              )).join(' ')
+            }
           </div>
         ))
       }
