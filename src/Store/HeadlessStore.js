f2289856b54bfd2b0583d8b01ab59c9d1d291b0f
Ethan Clark <ethangclark@gmail.com>
2016-10-19 08:43:40 -0600

    Cleaning up

--- src/Store/HeadlessStore.js
+++
@@ -1,9 +0,0 @@
-import Store from './Store'
-
-export default function HeadlessStore(stateDef) {
-
-  const headlessStore = Store(stateDef)
-  headlessStore._setEffectingSubscriber((_, triggerEffects) => triggerEffects())
-
-  return headlessStore
-}
